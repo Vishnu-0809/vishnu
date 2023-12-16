6df66260f05d5b43d74767923daf93c8eb313123
wasimk0077 <wkhan5776@gmail.com>
2023-12-16 21:02:07 +0530

    newtncPrivaypolicy3

--- lib/screens/purchase_history_filters/filter2.dart
+++ lib/screens/purchase_history_filters/filter2.dart
@@ -1226,28 +1226,7 @@ Navigator.of(context).push(MaterialPageRoute(
                  
                   final sea = SuggestionsDetailsList[index];
                   // String WarrantyDate= " 0 days ";
-                     String current_Date=DateTime.now().toString().substring(0,10);
-      print("qqqqqqqqqqqqqqqqqqqqqqqq "+current_Date);
-
-        String Purchase_Date=sea.details["purchaseDate"].toString().substring(0,10);
-        print("qqqqqqqqqqqqqqqqqqqqqqqq "+Purchase_Date);
-
-
-        DateTime endDate = DateTime(int.parse(current_Date.substring(0,4)), int.parse(current_Date.substring(5,7)), int.parse(current_Date.substring(8,10)));
        
-  DateTime startDate = DateTime(int.parse(Purchase_Date.substring(0,4)), int.parse(Purchase_Date.substring(5,7)), int.parse(Purchase_Date.substring(8,10)));
-       print(startDate);
-
-       Duration difference = endDate.difference(startDate);
-       int numberOfDays = difference.inDays;
-      print(numberOfDays);
-      int Warranty_Left= int.parse(sea.details["warranty"].toString())-numberOfDays;
-     print(Warranty_Left);
-                       
-if(Warranty_Left<0)
-{
-  Warranty_Left=0;
-}
                   // if(sea.details["warranty"]!=null)
                   // {
                   //   if(((sea.details["warranty"])/30).toInt()==0)
@@ -1497,7 +1476,7 @@ if(Warranty_Left<0)
                                               // ),
                                               FittedBox(
                                                 fit: BoxFit.fitWidth,
-                                                child: sea.details["warranty"]==null?
+                                                child: sea.details["warrantyPeriod"]==null?
                                                 Text(
                                     "Warranty of" +" 0 days "+ "applicable",
                                     style: TextStyle(
@@ -1511,7 +1490,7 @@ if(Warranty_Left<0)
                                               0.014,
                                     ),
                                   ):Text(
-                                    "Warranty of" +" "+sea.details["warranty"].toString()+ " days applicable",
+                                    "Warranty of" +" "+sea.details["warrantyPeriod"].toString()+ " days applicable",
                                     style: TextStyle(
                                       // fontWeight: FontWeight.bold,
                                       fontFamily: "Poppins Medium",
