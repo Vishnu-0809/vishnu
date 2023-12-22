c5e60067287ee8bd00fc562e4fc2bf072342e6a6
wasimk0077 <wkhan5776@gmail.com>
2023-12-22 12:05:57 +0530

    forgotpwdunfixed2

--- lib/screens/final_forgot_password.dart
+++ lib/screens/final_forgot_password.dart
@@ -60,10 +60,10 @@ bool visible_confirm =true;
         //     builder: (context) => HomeScreen(first_time: 1,),
         //   ),
         // );
-         Navigator.of(context).push(
+         Navigator.of(context).pushAndRemoveUntil(
                                       MaterialPageRoute(
                                           builder: ((context) =>Login(mainLink: widget.main_Link,location_on: true,))),
-                                      // (Route<dynamic> route) => false,
+                                      (Route<dynamic> route) => false,
                                       );
       } else if(token3!.status==400)
       {
